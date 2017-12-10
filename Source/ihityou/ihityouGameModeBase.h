1118a7a6f3f2cb4df1e4aa6f7dc70755cb40a1bb
YunshanJiang <yunshantree@163.com>
2017-12-09 21:39:51 -0500

    Initial commit

---
+++ Source/ihityou/ihityouGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ihityouGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IHITYOU_API AihityouGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
